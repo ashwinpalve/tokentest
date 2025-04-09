7a3c526e82aea6c4d50b13a2b45ae38384d3db5f
Ashwin Palve <ashwinpalve@Ashwins-MacBook-Pro-2.local>
2025-04-09 14:20:07 +0530

    Create custom tokenizer for hindi language using unicode

---
+++ customtokenizer.py
@@ -0,0 +1,24 @@
+import unicodedata
+
+def hindi_tokenizer(text):
+    """Basic Tokenizer for splitting text into unicode points"""
+    normalized=unicodedata.normalize('NFC',text)
+    return [ord(char) for char in normalized]
+
+def decode_tokens(tokens):
+    """Convert token codes back to text"""
+    return ''.join(chr(token) for token in tokens)
+
+
+
+# Hindi text sample
+
+hindi_text = "यह एक हिंदी भाषा का उदाहरण है।"
+hindi_tokens = hindi_tokenizer(hindi_text)
+decoded_text = decode_tokens(hindi_tokens)
+
+print("\nHindi text tokens\n", hindi_tokens)
+
+print("\nDecoded tokens\n", decoded_text)
+
+
