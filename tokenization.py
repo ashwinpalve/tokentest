012f1d368c437d98e84a059ee25e7962cb9d8b66
Ashwin Palve <ashwinpalve@Ashwins-MacBook-Pro-2.local>
2025-04-09 14:00:20 +0530

    Update tokentest

--- tokenization.py
+++ tokenization.py
@@ -5,12 +5,24 @@ encoder = tiktoken.encoding_for_model('gpt-4o')
 
 print("Vocab Size", encoder.n_vocab)
 
-text = "Much Ado about nothing"
+text = "Much Ado about nothing\n"
 tokens = encoder.encode(text)
 
-print("Tokens", tokens)
+print("\nTokens", tokens)
 
 mytokens = [54041, 355, 2408, 1078, 6939]
 decoded = encoder.decode(mytokens)
 
-print("Decoded", decoded)
+print("\nDecoded", decoded)
+
+# Hindi Text tokenization 
+
+# Hindi text sample
+hindi_text = "यह एक हिंदी भाषा का उदाहरण है।"
+hindi_tokens = encoder.encode(hindi_text)
+print("\nHindi text tokens\n", hindi_tokens)
+
+# Full decoding
+decoded_text = encoder.decode(hindi_tokens)
+print("\nDecoded Text:\n", decoded_text)
+
